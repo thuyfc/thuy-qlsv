a8ab5993e4f05a2a6405ef318f6b4ae3e58cb381
thuyfc <nguyenthuy998lc@gmail.com>
2022-07-05 11:00:54 +0700

    đ

--- src/Login/Account.js
+++ src/Login/Account.js
@@ -1,35 +1,35 @@
-// import React, { useState } from "react"
+import React, { useState } from "react"
 
-// import { useAuth } from "../context/AuthContext"
-// import { Link, useNavigate } from "react-router-dom"
+import { useAuth } from "../context/AuthContext"
+import { Link, useNavigate } from "react-router-dom"
 
-// export default function Account() {
-//   const [error, setError] = useState("")
-//   const { currentUser, logout } = useAuth()
-//   const navigate = useNavigate()
-//    async function handleLogout() {
-//     setError("")
-//     try {
-//       await logout()
-//      navigate("/login")
-//     } catch {
-//       setError("Failed to log out")
-//     }
-//   }
+export default function Account() {
+  const [error, setError] = useState("")
+  const { currentUser, logout } = useAuth()
+  const navigate = useNavigate()
+   async function handleLogout() {
+    setError("")
+    try {
+      await logout()
+     navigate("/login")
+    } catch {
+      setError("Failed to log out")
+    }
+  }
 
-//   return (
-//     <>
-//       <div className='max-w-[600px] mx-auto my-16 p-4 w-40 h-60 rounded-lg bg-slate-100'>
-//         <h1 className='text-2xl font-bold py-4'>Account</h1>
-//         <strong>Email:</strong> {currentUser && currentUser.email}
-//         <br />
+  return (
+    <>
+      <div className='max-w-[600px] mx-auto my-16 p-4 w-40 h-60 rounded-lg bg-slate-100'>
+        <h1 className='text-2xl font-bold py-4'>Account</h1>
+        <strong>Email:</strong> {currentUser && currentUser.email}
+        <br />
 
 
-//         <button onClick={handleLogout} className='border px-6 py-2 my-4'>
-//           Logout
-//         </button>
-//       </div>
+        <button onClick={handleLogout} className='border px-6 py-2 my-4'>
+          Logout
+        </button>
+      </div>
 
-//     </>
-//   )
-// }
+    </>
+  )
+}
